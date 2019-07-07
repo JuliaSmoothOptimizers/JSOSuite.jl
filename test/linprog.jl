62fd27a0234b6c65f9cca8b87e806af46f6d4e37
RenanOD <renan_oclides@hotmail.com>
2019-07-07 17:41:55 -0300

    create test file for linprog

---
+++ test/linprog.jl
@@ -0,0 +1,11 @@
+function linprog_tests()
+  c = [-1.; -1/3]
+  A = [1. 1; 1 1/4; 1 -1; -1/4 -1; -1 -1; -1 1]
+  b = [2.; 1; 2; 1; -1; 2]
+  sol = [1/3; 4/3]
+  output = linprog(c, A, b)
+  @test output.dual_feas < 1e-6
+  @test norm(output.solution - sol) < 1e-6
+end
+
+linprog_tests()

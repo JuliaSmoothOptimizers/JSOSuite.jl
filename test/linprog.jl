4c6c1eff398aeac88e2f82c2db573e98fa5451ed
RenanOD <renan_oclides@hotmail.com>
2019-07-07 17:50:45 -0300

    fix reference solution for linprog test

--- test/linprog.jl
+++ test/linprog.jl
@@ -2,7 +2,7 @@ function linprog_tests()
   c = [-1.; -1/3]
   A = [1. 1; 1 1/4; 1 -1; -1/4 -1; -1 -1; -1 1]
   b = [2.; 1; 2; 1; -1; 2]
-  sol = [1/3; 4/3]
+  sol = [2/3; 4/3]
   output = linprog(c, A, b)
   @test output.dual_feas < 1e-6
   @test norm(output.solution - sol) < 1e-6

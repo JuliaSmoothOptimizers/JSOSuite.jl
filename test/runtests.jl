5c51b14d7bb7aa45768f15ef80b8e77497f7cce4
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-07-06 22:01:59 -0300

    Start minimize API

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,6 @@ using NLPModels
 # stdlib
 using LinearAlgebra, Test
 
-function tests()
-  nlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0])
-  output = minimize(nlp)
-  @test norm(output.solution .- 1) < 1e-8
-  @test output.dual_feas < 1e-8
 end
 
-tests()
+include("minimize.jl")

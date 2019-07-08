84ac7be4550a6963fbc4ddc8b2bc68a154b91266
RenanOD <renan_oclides@hotmail.com>
2019-07-08 15:47:14 -0300

    Resolved merge conflict

--- src/linprog.jl
+++ src/linprog.jl
@@ -14,3 +14,13 @@ function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector)
   output = ipopt(nlp, print_level=0)
   return output
 end
+
+function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector, Aeq::MatrixOrOperator, beq::AbstractVector)
+  f(x) = dot(c, x)
+  con(x) = [A*x; Aeq*x]
+  lcon = [fill(-Inf, length(b)); beq]
+  ucon = [b; beq]
+  nlp = ADNLPModel(f, zeros(length(c)), c = con, lcon = lcon, ucon = ucon)
+  output = ipopt(nlp)
+  return output
+end
--- test/linprog.jl
+++ test/linprog.jl
@@ -2,14 +2,28 @@ function linprog_tests()
   @testset "linprog" begin
     c = [-1.; -1/3]
     A = [1. 1; 1 1/4; 1 -1; -1/4 -1; -1 -1; -1 1]
+    opA = LinearOperator(A)
     b = [2.; 1; 2; 1; -1; 2]
     sol = [2/3; 4/3]
+
     output = linprog(c, A, b)
     @test output.dual_feas < 1e-6
     @test norm(output.solution - sol) < 1e-6
 
-    opA = LinearOperator(A)
     output = linprog(c, opA, b)
+    @test output.dual_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    Aeq = [1 1/4]
+    opAeq = LinearOperator(Aeq)
+    beq = [1/2]
+    sol = [0; 2]
+
+    output = linprog(c, A, b, Aeq, beq)
+    @test output.dual_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    output = linprog(c, opA, b, opAeq, beq)
     @test output.dual_feas < 1e-6
     @test norm(output.solution - sol) < 1e-6
   end

963369288dd19d476bf44984e66ee632b5adc02e
Renan Domingues <renan_oclides@hotmail.com>
2019-07-10 19:55:53 -0300

    Add linprog methods (#9)
    * add linprog method for variable inequalities
    
    * simplify linprog methods in two functions, allow keyword arguments
    
    * make single a linprog function with all optional constraints
    
    * check size consistency
    
    * added missing methods
    
    * fix alignment, add primal.feas to tests, simplify linprog's expression

--- src/linprog.jl
+++ src/linprog.jl
@@ -1,16 +1,76 @@
 export linprog
 
-const MatrixOrOperator = Union{AbstractMatrix,AbstractLinearOperator}
+const MatrixOrOperator = Union{AbstractMatrix, AbstractLinearOperator}
 
 """
-    linprog(c, A, b)
+    linprog(c; x0, Aineq, blow, bupp, Aeq, beq, lvar, uvar)
 
-Minimize the linear problem cᵀ x with constraints A x ≤ b.
+Minimize cᵀ x subject to any combination of the optional constraints
+blow ≤ Aineq x ≤ bupp, Aeq x = beq and lvar ≤ x ≤ uvar.
+Initial solution is optional in all methods and set to zeros by default.
 """
-function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector)
+function linprog(c::AbstractVector; x0::AbstractVector = zeros(length(c)), Aineq::MatrixOrOperator = zeros(0, length(c)),
+                 blow::AbstractVector = fill(-Inf, size(Aineq, 1)), bupp::AbstractVector = fill(Inf, size(Aineq, 1)),
+                 lvar::AbstractVector = fill(-Inf, length(c)), uvar::AbstractVector = fill(Inf, length(c)),
+                 Aeq::MatrixOrOperator = zeros(0, length(c)), beq::AbstractVector = eltype(x0)[])
+  nvar = length(c)
+  @assert length(lvar) == length(uvar) == nvar
+  @assert size(Aineq, 2) == size(Aeq, 2) == nvar
+  @assert size(Aineq, 1) == length(blow) && size(Aineq, 1) == length(bupp)
+  @assert size(Aeq, 1) == length(beq)
+
   f(x) = dot(c, x)
-  con(x) = A*x
-  nlp = ADNLPModel(f, zeros(length(c)), c = con, ucon = b)
-  output = ipopt(nlp, print_level=0)
+  con = x -> [Aineq * x; Aeq * x]
+  nlp = ADNLPModel(f, x0, c = con, lcon = [blow; beq], ucon = [bupp; beq], lvar = lvar, uvar = uvar)
+  output = ipopt(nlp, print_level = 0)
   return output
+end
+
+"""
+    linprog(c, A, b; x0)
+
+Minimize cᵀ x subject to A x ≤ b.
+"""
+function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector; x0::AbstractVector = zeros(length(c)))
+  return linprog(c, Aineq = A, bupp = b, x0 = x0)
+end
+
+"""
+    linprog(c, A, b, Aeq, beq; x0)
+
+Minimize cᵀ x subject to A x ≤ b and Aeq x = beq.
+"""
+function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector, Aeq::MatrixOrOperator, beq::AbstractVector;
+                 x0::AbstractVector = zeros(length(c)))
+  return linprog(c, Aineq = A, bupp = b, Aeq = Aeq, beq = beq, x0 = x0)
+end
+
+"""
+    linprog(c, A, b, lvar, uvar; x0)
+
+Minimize cᵀ x subject to A x ≤ b and lvar ≤ x ≤ uvar.
+"""
+function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector,
+                 lvar::AbstractVector, uvar::AbstractVector; x0::AbstractVector = zeros(length(c)))
+  return linprog(c, Aineq = A, bupp = b, lvar = lvar, uvar = uvar, x0 = x0)
+end
+
+"""
+    linprog(c, A, b, Aeq, beq, lvar, uvar; x0)
+
+Minimize cᵀ x subject to A x ≤ b, Aeq x = beq and lvar ≤ x ≤ uvar.
+"""
+function linprog(c::AbstractVector, A::MatrixOrOperator, b::AbstractVector, Aeq::MatrixOrOperator,
+                 beq::AbstractVector, lvar::AbstractVector, uvar::AbstractVector; x0::AbstractVector = zeros(length(c)))
+  return linprog(c, Aineq = A, bupp = b, Aeq = Aeq, beq = beq, lvar = lvar, uvar = uvar, x0 = x0)
+end
+
+"""
+    linprog(c, Aineq, blow, bupp; x0)
+
+Minimize cᵀ x subject to blow ≤ Aineq x ≤ bupp.
+"""
+function linprog(c::AbstractVector, Aineq::MatrixOrOperator, blow::AbstractVector, bupp::AbstractVector;
+                 x0::AbstractVector = zeros(length(c)))
+  return linprog(c, Aineq = Aineq, blow = blow, bupp = bupp, x0 = x0)
 end
--- test/linprog.jl
+++ test/linprog.jl
@@ -1,16 +1,53 @@
 function linprog_tests()
   @testset "linprog" begin
     c = [-1.; -1/3]
-    A = [1. 1; 1 1/4; 1 -1; -1/4 -1; -1 -1; -1 1]
-    b = [2.; 1; 2; 1; -1; 2]
+    Aineq = [1. 1; 1 1/4; 1 -1; -1/4 -1; -1 -1; -1 1]
+    opAineq = LinearOperator(Aineq)
+    bupp = [2.; 1; 2; 1; -1; 2]
+    blow = fill(-Inf, length(bupp))
     sol = [2/3; 4/3]
-    output = linprog(c, A, b)
+
+    output = linprog(c, Aineq, bupp)
     @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
     @test norm(output.solution - sol) < 1e-6
 
-    opA = LinearOperator(A)
-    output = linprog(c, opA, b)
+    output = linprog(c, Aineq, blow, bupp)
     @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    Aeq = [1 1/4]
+    opAeq = LinearOperator(Aeq)
+    beq = [1/2]
+    sol = [0; 2]
+
+    output = linprog(c, Aineq, bupp, Aeq, beq)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    lvar = [-1.; -1/2]
+    uvar = [3/2; 5/4]
+    sol = [11/16; 5/4]
+
+    output = linprog(c, Aineq, bupp, lvar, uvar)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    sol = [3/16; 5/4]
+
+    output = linprog(c, Aineq, bupp, Aeq, beq, lvar, uvar)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    x0 = [1.; -2.]
+
+    output = linprog(c, Aineq = opAineq, blow = blow, bupp = bupp, Aeq = opAeq, beq = beq, lvar = lvar, uvar = uvar, x0 = x0)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
     @test norm(output.solution - sol) < 1e-6
   end
 end

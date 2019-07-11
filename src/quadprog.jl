5fbab523508193856ce63456b50e098028e96f6e
RenanOD <renan_oclides@hotmail.com>
2019-07-11 14:50:21 -0300

    add quadprog interfaces

--- src/quadprog.jl
+++ src/quadprog.jl
@@ -1,13 +1,83 @@
 export quadprog
 
+const MatrixOrOperator = Union{AbstractMatrix, AbstractLinearOperator}
+
 """
-    quadprog(Q, g, A, b)
+    quadprog(Q, c; x0, Aineq, blow, bupp, Aeq, beq, lvar, uvar)
 Minimize the quadratic problem gᵀ x + ½ xᵀ Q x subject to A*x ≤ b.
 """
-function quadprog(Q, g, A, b)
+function quadprog(Q::AbstractMatrix, g::AbstractVector;
+                  x0::AbstractVector = zeros(length(g)), Aineq::MatrixOrOperator = zeros(0, length(g)),
+                  blow::AbstractVector = fill(-Inf, size(Aineq, 1)), bupp::AbstractVector = fill(Inf, size(Aineq, 1)),
+                  lvar::AbstractVector = fill(-Inf, length(g)), uvar::AbstractVector = fill(Inf, length(g)),
+                  Aeq::MatrixOrOperator = zeros(0, length(g)), beq::AbstractVector = eltype(x0)[])
+  nvar = length(g)
+  @assert size(Q, 1) == size(Q, 2) == nvar
+  @assert length(lvar) == length(uvar) == nvar
+  @assert size(Aineq, 2) == size(Aeq, 2) == nvar
+  @assert size(Aineq, 1) == length(blow) && size(Aineq, 1) == length(bupp)
+  @assert size(Aeq, 1) == length(beq)
   f(x) = dot(x, Q*x)/2 + dot(g,x)
-  con(x) = A*x
-  nlp = ADNLPModel(f, zeros(length(g)), c = con, ucon = b)
+  con = x -> [Aineq * x; Aeq * x]
+  nlp = ADNLPModel(f, x0, c = con, lcon = [blow; beq], ucon = [bupp; beq], lvar = lvar, uvar = uvar)
   output = ipopt(nlp, print_level=0)
   return output
+end
+
+"""
+    quadprog(Q, g; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x without any constraints.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, A::MatrixOrOperator, b::AbstractVector; x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, x0 = x0)
+end
+
+"""
+    quadprog(Q, g, A, b; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x subject to A x ≤ b.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, A::MatrixOrOperator, b::AbstractVector; x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, Aineq = A, bupp = b, x0 = x0)
+end
+
+"""
+    quadprog(Q, g, A, b, Aeq, beq; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x subject to A x ≤ b and Aeq x = beq.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, A::MatrixOrOperator, b::AbstractVector, Aeq::MatrixOrOperator, beq::AbstractVector;
+                  x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, Aineq = A, bupp = b, Aeq = Aeq, beq = beq, x0 = x0)
+end
+
+"""
+    quadprog(Q, g, A, b, lvar, uvar; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x subject to A x ≤ b and lvar ≤ x ≤ uvar.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, A::MatrixOrOperator, b::AbstractVector,
+                 lvar::AbstractVector, uvar::AbstractVector; x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, Aineq = A, bupp = b, lvar = lvar, uvar = uvar, x0 = x0)
+end
+
+"""
+    quadprog(Q, g, A, b, Aeq, beq, lvar, uvar; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x subject to A x ≤ b, Aeq x = beq and lvar ≤ x ≤ uvar.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, A::MatrixOrOperator, b::AbstractVector, Aeq::MatrixOrOperator,
+                  beq::AbstractVector, lvar::AbstractVector, uvar::AbstractVector; x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, Aineq = A, bupp = b, Aeq = Aeq, beq = beq, lvar = lvar, uvar = uvar, x0 = x0)
+end
+
+"""
+    quadprog(Q, g, Aineq, blow, bupp; x0)
+
+Minimize gᵀ x + ½ xᵀ Q x subject to blow ≤ Aineq x ≤ bupp.
+"""
+function quadprog(Q::AbstractMatrix, g::AbstractVector, Aineq::MatrixOrOperator, blow::AbstractVector, bupp::AbstractVector;
+                  x0::AbstractVector = zeros(length(g)))
+  return quadprog(Q, g, Aineq = Aineq, blow = blow, bupp = bupp, x0 = x0)
 end
--- test/quadprog.jl
+++ test/quadprog.jl
@@ -2,11 +2,60 @@ function quadprog_tests()
   @testset "quadprog" begin
     Q = [1. -1; -1 2]
     g = [-2.; -6]
-    A = [1. 1; -1 2; 2 1]
-    b = [2.; 2; 3]
-    sol = [2/3; 4/3]
-    output = quadprog(Q, g, A, b)
+    sol = [10.; 8]
+
+    output = quadprog(Q, g)
     @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    Aineq = [1. 1; -1 2; 2 1]
+    opAineq = LinearOperator(Aineq)
+    bupp = [2.; 2; 3]
+    blow = fill(-Inf, length(bupp))
+    sol = [2/3; 4/3]
+
+    output = quadprog(Q, g, Aineq, bupp)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    output = quadprog(Q, g, Aineq, blow, bupp)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    Aeq = [1 1]
+    opAeq = LinearOperator(Aeq)
+    beq = [0]
+    sol = [-2/3; 2/3]
+
+    output = quadprog(Q, g, Aineq, bupp, Aeq, beq)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    lvar = [0.; 0]
+    uvar = [1.; 1]
+    sol = [1.; 1]
+
+    output = quadprog(Q, g, Aineq, bupp, lvar, uvar)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    sol = [0.; 0]
+
+    output = quadprog(Q, g, Aineq, bupp, Aeq, beq, lvar, uvar)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
+    @test norm(output.solution - sol) < 1e-6
+
+    x0 = [1.; -2.]
+
+    output = quadprog(Q, g, Aineq = opAineq, blow = blow, bupp = bupp, Aeq = opAeq, beq = beq, lvar = lvar, uvar = uvar, x0 = x0)
+    @test output.dual_feas < 1e-6
+    @test output.primal_feas < 1e-6
     @test norm(output.solution - sol) < 1e-6
   end
 end

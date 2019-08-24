f6f38b491574256a459e17f7fe641a4c0daa0836
RenanOD <renan_oclides@hotmail.com>
2019-08-24 01:37:05 -0300

    Add test with non function type

--- test/minimize.jl
+++ test/minimize.jl
@@ -1,3 +1,13 @@
+mutable struct LinearRegression
+  x :: Vector
+  y :: Vector
+end
+
+function (regr::LinearRegression)(beta)
+  r = regr.y .- beta[1] - beta[2] * regr.x
+  return dot(r, r) / 2
+end
+
 function minimize_tests()
   f(x) = (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
   x₀ = [-1.2; 1.0]
@@ -17,6 +27,16 @@ function minimize_tests()
 
   output = minimize(f, x₀, fill(1.1, 2), fill(1.9, 2), print_level=0)
   @test norm(output.solution .- [1.1; 1.21]) < 1e-6
+  @test output.dual_feas < 1e-6
+
+  x = range(-1, 1, length=100)
+  y = 2x .+ 3 + randn(100) * 0.1
+  regr = LinearRegression(x, y)
+  nlp = ADNLPModel(regr, x₀)
+  β = [ones(100) x] \ y
+
+  output = minimize(nlp, print_level=0)
+  @test abs(output.objective - norm(y .- β[1] - β[2] * x)^2 / 2) < 1e-12
   @test output.dual_feas < 1e-6
 end
 

3655aae1a45213c51fd22313406961156b317dc9
RenanOD <renan_oclides@hotmail.com>
2019-10-25 01:10:07 -0300

    give minimize a testset

--- test/minimize.jl
+++ test/minimize.jl
@@ -9,42 +9,44 @@ function (regr::LinearRegression)(beta)
 end
 
 function minimize_tests()
-  f(x) = (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
-  x₀ = [-1.2; 1.0]
+  @testset "linprog" begin
+    f(x) = (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
+    x₀ = [-1.2; 1.0]
 
-  nlp = ADNLPModel(f, x₀)
-  output = minimize(nlp, print_level=0)
-  @test norm(output.solution .- 1) < 1e-6
-  @test output.dual_feas < 1e-6
+    nlp = ADNLPModel(f, x₀)
+    output = minimize(nlp, print_level=0)
+    @test norm(output.solution .- 1) < 1e-6
+    @test output.dual_feas < 1e-6
 
-  output = minimize(f, x₀, print_level=0)
-  @test norm(output.solution .- 1) < 1e-6
-  @test output.dual_feas < 1e-6
+    output = minimize(f, x₀, print_level=0)
+    @test norm(output.solution .- 1) < 1e-6
+    @test output.dual_feas < 1e-6
 
-  output = minimize(f, x₀, fill(0.1, 2), fill(0.5, 2), print_level=0)
-  @test norm(output.solution .- [0.5; 0.25]) < 1e-6
-  @test output.dual_feas < 1e-6
+    output = minimize(f, x₀, fill(0.1, 2), fill(0.5, 2), print_level=0)
+    @test norm(output.solution .- [0.5; 0.25]) < 1e-6
+    @test output.dual_feas < 1e-6
 
-  output = minimize(f, x₀, fill(1.1, 2), fill(1.9, 2), print_level=0)
-  @test norm(output.solution .- [1.1; 1.21]) < 1e-6
-  @test output.dual_feas < 1e-6
+    output = minimize(f, x₀, fill(1.1, 2), fill(1.9, 2), print_level=0)
+    @test norm(output.solution .- [1.1; 1.21]) < 1e-6
+    @test output.dual_feas < 1e-6
 
-  kwargs = (x0 = [-3.0; 1.0],)
+    kwargs = (x0 = [-3.0; 1.0],)
 
-  nlp = ADNLPModel(f, x₀)
-  output = minimize(nlp, print_level=0; kwargs...)
-  @test norm(output.solution .- 1) < 1e-6
-  @test output.dual_feas < 1e-6
+    nlp = ADNLPModel(f, x₀)
+    output = minimize(nlp, print_level=0; kwargs...)
+    @test norm(output.solution .- 1) < 1e-6
+    @test output.dual_feas < 1e-6
 
-  x = range(-1, 1, length=100)
-  y = 2x .+ 3 + randn(100) * 0.1
-  regr = LinearRegression(x, y)
-  nlp = ADNLPModel(regr, x₀)
-  β = [ones(100) x] \ y
+    x = range(-1, 1, length=100)
+    y = 2x .+ 3 + randn(100) * 0.1
+    regr = LinearRegression(x, y)
+    nlp = ADNLPModel(regr, x₀)
+    β = [ones(100) x] \ y
 
-  output = minimize(nlp, print_level=0)
-  @test abs(output.objective - norm(y .- β[1] - β[2] * x)^2 / 2) < 1e-12
-  @test output.dual_feas < 1e-6
+    output = minimize(nlp, print_level=0)
+    @test abs(output.objective - norm(y .- β[1] - β[2] * x)^2 / 2) < 1e-12
+    @test output.dual_feas < 1e-6
+  end
 end
 
 minimize_tests()

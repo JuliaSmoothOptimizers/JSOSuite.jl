3a9b48f694e3bb6b3aeb55473ae268e480063d77
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-04-14 20:48:25 -0400

    adding/updating file: docs/make.jl

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using Documenter, JSOSuite
+
+makedocs(
+  modules = [JSOSuite],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(assets = ["assets/style.css"], prettyurls = get(ENV, "CI", nothing) == "true"),
+  sitename = "JSOSuite.jl",
+  pages = Any["Home" => "index.md",
+              "Tutorial" => "tutorial.md",
+              "Reference" => "reference.md"]
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/JSOSuite.jl.git")

728e925cad53a19836892265f332c9390c0e83c1
tmigot <tangi.migot@gmail.com>
2022-08-22 14:25:00 +0200

    remove old files

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,11 +6,3 @@ using LinearOperators, NLPModels
 
 # stdlib
 using LinearAlgebra, Test
-
-include("linprog.jl")
-include("quadprog.jl")
-
-include("minimize.jl")
-include("fminunc.jl")
-include("fminbnd.jl")
-include("fmincon.jl")

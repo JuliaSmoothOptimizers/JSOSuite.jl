b5346f291f6fa238a222f8c92dde2cba3dc78139
tmigot <tmigot@users.noreply.github.com>
2022-09-01 14:59:06 +0000

    :robot: Format .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,7 +21,7 @@ for solver in eachrow(JSOSuite.solvers)
   @testset "Test options in $(solver.name)" begin
     # We just test that the solver runs with the options
     if solver.is_available && solver.can_solve_nlp
-      solve(nlp, solver.name, atol = 1e-5, rtol=1e-5, max_time = 12.0, max_eval = 10, verbose = 0)
+      solve(nlp, solver.name, atol = 1e-5, rtol = 1e-5, max_time = 12.0, max_eval = 10, verbose = 0)
       @test true
     end
   end

3edbdd810829986c85f99c111fda3e12cece9229
tmigot <tangi.migot@gmail.com>
2022-11-02 09:33:04 -0400

    add tests with nlsproblems (#62)
    add nls unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 using JSOSuite
 
 # JSO
-using ADNLPModels, NLPModels, QuadraticModels, OptimizationProblems
+using ADNLPModels, NLPModels, NLSProblems, QuadraticModels, OptimizationProblems
 
 # stdlib
 using LinearAlgebra, Test
@@ -11,11 +11,44 @@ meta = OptimizationProblems.meta
 @testset "Test solve on OptimizationProblems" begin
   for name in meta[meta.nvar .< 100, :name]
     nlp = OptimizationProblems.ADNLPProblems.eval(Meta.parse(name))()
-    solve(nlp)
+    solve(nlp, verbose = 0)
     @test true
     model = OptimizationProblems.PureJuMP.eval(Meta.parse(name))()
-    solve(model)
+    solve(model, verbose = 0)
     @test true
+  end
+end
+
+for solver in eachrow(JSOSuite.solvers)
+  nlp = mgh17()
+  @testset "Test options in $(solver.name)" begin
+    # We just test that the solver runs with the options
+    if solver.is_available
+      if solver.nonlinear_obj
+        solve(
+          nlp,
+          solver.name,
+          atol = 1e-5,
+          rtol = 1e-5,
+          max_time = 12.0,
+          max_eval = 10,
+          verbose = 0,
+        )
+        @test true
+      else
+        nlp_qm = QuadraticModel(nlp, nlp.meta.x0)
+        solve(
+          nlp_qm,
+          solver.name,
+          atol = 1e-5,
+          rtol = 1e-5,
+          max_time = 12.0,
+          max_eval = 10,
+          verbose = 0,
+        )
+        @test true
+      end
+    end
   end
 end
 

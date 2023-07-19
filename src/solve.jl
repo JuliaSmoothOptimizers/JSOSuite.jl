9fae05f9de6e4b65aae9031781aa4020ceac4aa2
tmigot <tangi.migot@gmail.com>
2023-07-19 15:53:05 -0400

    add `max_time` for RipQP

--- src/solve.jl
+++ src/solve.jl
@@ -106,7 +106,7 @@ function solve(::Val{:IPOPT}, nlp; kwargs...)
   return ipopt(nlp; keywords...)
 end
 
-function solve(::Val{:RipQP}, nlp::Union{QuadraticModel{T0}, LLSModel{T0}}; max_iter = 200, kwargs...) where {T0}
+function solve(::Val{:RipQP}, nlp::Union{QuadraticModel{T0}, LLSModel{T0}}; max_iter = 200, max_time = 1200.0, kwargs...) where {T0}
   keywords = Dict(kwargs)
   if :verbose in keys(keywords)
     keywords[:display] = convert(Bool, keywords[:verbose])
@@ -117,23 +117,19 @@ function solve(::Val{:RipQP}, nlp::Union{QuadraticModel{T0}, LLSModel{T0}}; max_
     ϵ_rb = ϵ_rc = T0(keywords[:atol])
     delete!(keywords, :atol)
     delete!(keywords, :rtol)
-    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter)
+    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter, max_time = max_time)
   elseif :atol in keys(keywords)
     ϵ_pdd = T0(keywords[:rtol])
     ϵ_rb = ϵ_rc = T0(keywords[:atol])
     delete!(keywords, :atol)
-    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter)
+    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter, max_time = max_time)
   elseif :rtol in keys(keywords)
     ϵ_pdd = T0(keywords[:rtol])
     ϵ_rb = ϵ_rc = T0(keywords[:atol])
     delete!(keywords, :rtol)
-    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter)
+    RipQP.InputTol(T0, ϵ_pdd = ϵ_pdd, ϵ_rb = ϵ_rb, ϵ_rc = ϵ_rc, max_iter = max_iter, max_time = max_time)
   else
     RipQP.InputTol(T0)
-  end
-  if :max_time in keys(keywords)
-    @warn "Not implemented option `max_time` for RipQP."
-    delete!(keywords, :max_time)
   end
   if :max_eval in keys(keywords)
     @warn "Not implemented option `max_eval` for RipQP"

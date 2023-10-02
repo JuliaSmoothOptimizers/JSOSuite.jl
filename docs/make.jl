31036457209a8ba477688a42c5fe5cc54d84d91c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2023-10-02 17:32:25 +0200

    Apply COPIERTemplate.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,31 @@
-using Documenter, JSOSuite
+using JSOSuite
+using Documenter
 
-makedocs(
+DocMeta.setdocmeta!(JSOSuite, :DocTestSetup, :(using JSOSuite); recursive = true)
+
+makedocs(;
   modules = [JSOSuite],
   doctest = true,
-  # linkcheck = true,
-  strict = true,
-  format = Documenter.HTML(
-    assets = ["assets/style.css"],
-    prettyurls = get(ENV, "CI", nothing) == "true",
-  ),
+  linkcheck = true,
+  authors = "Tangi Migot <tangi.migot@gmail.com> and contributors",
+  repo = "https://github.com/JuliaSmoothOptimizers/JSOSuite.jl/blob/{commit}{path}#{line}",
   sitename = "JSOSuite.jl",
-  pages = Any[
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://JuliaSmoothOptimizers.github.io/JSOSuite.jl",
+    assets = ["assets/style.css"],
+  ),
+  pages = [
     "Home" => "index.md",
     "Tutorial" => "tutorial.md",
     "Nonlinear Least Squares" => "nls.md",
     "Quadratic models with linear constraints" => "qp.md",
     "Benchmarking" => "benchmark.md",
     "Speed up Solvers Tips" => "speed-up.md",
+    "Contributing" => "contributing.md",
+    "Dev setup" => "developer.md",
     "Reference" => "reference.md",
   ],
 )
 
-deploydocs(
-  repo = "github.com/JuliaSmoothOptimizers/JSOSuite.jl.git",
-  push_preview = true,
-  devbranch = "main",
-)
+deploydocs(; repo = "github.com/JuliaSmoothOptimizers/JSOSuite.jl", push_preview = true)

a9996774552f0335b773392ee2ef03a1048b0a10
tmigot <tangi.migot@gmail.com>
2024-11-02 10:46:48 -0400

    apply pre-commit

--- .breakage/get_jso_users.jl
+++ .breakage/get_jso_users.jl
@@ -1,9 +1,10 @@
-import GitHub, PkgDeps  # both export users()
+using GitHub: GitHub
+using PkgDeps: PkgDeps  # both export users()
 
 length(ARGS) >= 1 || error("specify at least one JSO package as argument")
 
 jso_repos, _ = GitHub.repos("JuliaSmoothOptimizers")
-jso_names = [splitext(x.name)[1] for x ∈ jso_repos]
+jso_names = [splitext(x.name)[1] for x in jso_repos]
 
 name = splitext(ARGS[1])[1]
 name ∈ jso_names || error("argument should be one of ", jso_names)
